6a7aa9b7f049489728d33a75540178e070c1a280
Jeff Bradberry <jeff.bradberry@gmail.com>
2013-01-09 16:26:19 -0500

    Initial project layout.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='ConnectFour',
+    version='0.1dev',
+    author='Jeff Bradberry',
+    author_email='jeff.bradberry@gmail.com',
+    packages=['connect_four'],
+    license='LICENSE',
+    description="An implementation of the game Connect Four.",
+)
