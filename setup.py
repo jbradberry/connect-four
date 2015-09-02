54b18920ca9df3fc1b244306e95a0fad398111e6
Jeff Bradberry <jeff.bradberry@gmail.com>
2015-09-02 19:53:56 -0400

    Added the board to the entry points.

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
     author='Jeff Bradberry',
     author_email='jeff.bradberry@gmail.com',
     packages=['connect_four'],
+    entry_points={
+        'jrb_board.games': 'connect_four.connectfour:Board',
+    },
     license='LICENSE',
     description="An implementation of the game Connect Four.",
 )
