33cff81cd51ef4360a975a13633f703c635650d3
Jeff Bradberry <jeff.bradberry@gmail.com>
2016-08-13 16:22:58 -0400

    Renamed legal_plays to legal_actions.
    Also,
    - renamed state_history to history
    - renamed play to action

--- connect_four/connectfour.py
+++ connect_four/connectfour.py
@@ -11,7 +11,7 @@ class Board(object):
     def start(self):
         return (0, 0, 1)
 
-    def display(self, state, play):
+    def display(self, state, action):
         p1, p2, player = state
 
         piece = {0: " ", 1: u"\u25cb", 2: u"\u25cf"}
@@ -19,7 +19,7 @@ class Board(object):
             " ".join(str(i) for i in xrange(self.cols)))
         bar = "  +{0}+".format("-"*(2*self.cols-1))
         msg = "Played: {0}\nPlayer {1} to move.".format(
-            self.pack(play), player)
+            self.pack(action), player)
 
         board = u"\n".join(
             u"  |{0}|".format(
@@ -31,30 +31,30 @@ class Board(object):
         board = u"\n".join((header, bar, board, bar, header, msg))
         return board
 
-    def parse(self, play):
-        return int(play)
+    def parse(self, action):
+        return int(action)
 
-    def pack(self, play):
-        return str(play)
+    def pack(self, action):
+        return str(action)
 
-    def is_legal(self, state_history, play):
-        p1, p2, player = state_history[-1]
+    def is_legal(self, history, action):
+        p1, p2, player = history[-1]
         occupied = p1 | p2
-        column = (occupied >> (self.rows * play)) & 0b111111
+        column = (occupied >> (self.rows * action)) & 0b111111
         return column <= 0b11111
 
-    def legal_plays(self, state_history):
-        p1, p2, player = state_history[-1]
+    def legal_actions(self, history):
+        p1, p2, player = history[-1]
         occupied = p1 | p2
         return [c for c in xrange(self.cols)
                 if (occupied >> (self.rows * c)) & 0b111111 <= 0b11111]
 
-    def next_state(self, state, play):
+    def next_state(self, state, action):
         p1, p2, player = state
         occupied = p1 | p2
-        column = (occupied >> (self.rows * play)) & 0b111111
+        column = (occupied >> (self.rows * action)) & 0b111111
         column += 1
-        column <<= self.rows * play
+        column <<= self.rows * action
         if player == 1:
             p1 |= column
         else:
@@ -64,8 +64,8 @@ class Board(object):
     def current_player(self, state):
         return state[-1]
 
-    def winner(self, state_history):
-        state = state_history[-1]
+    def winner(self, history):
+        state = history[-1]
         p1, p2, player = state
         occupied = p1 | p2
 
